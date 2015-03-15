5e571efc9b34752a962d11ce5df078156f067a71
Steve Coffman <gears@umich.edu>
2015-03-15 09:34:06 -0400

    Remove some stuff

---
+++ .dotfiles/atom/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Installing Atom and popular packages"
+brew cask install atom
+apm install Atom-Syntax-highlighting-for-Sass
+apm install Zen
+apm install ask-stack
+apm install atom-beautify
+apm install atom-bootstrap3
+apm install atom-css-comb
+apm install atom-html-preview
+apm install autoclose-html
+apm install autocomplete-plus
+apm install coffee-refactor
+apm install color-picker
+apm install file-icons
+apm install fixmyjs
+apm install fonts
+apm install git-log
+apm install git-plus
+apm install git-tab-status
+apm install linter
+apm install linter-csslint
+apm install linter-jscs
+apm install linter-jshint
+apm install linter-jsxhint
+apm install linter-scss-lint
+apm install linter-shellcheck
+apm install merge-conflicts
+apm install project-manager
+apm install react
+apm install refactor
+apm install regex-railroad-diagram
+apm install script
+apm install sort-lines
+apm install ssh-config
