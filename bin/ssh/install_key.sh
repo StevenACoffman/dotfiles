e36d7e23691f8506cff916c6cc17d21b0e991c36
Steve Coffman <gears@umich.edu>
2015-03-30 17:19:42 -0400

    fix install key to be friendlier

--- bin/ssh/install_key.sh
+++ bin/ssh/install_key.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "installing public key for $1"
-cat ~/.ssh/id_rsa.pub | ssh "$1" "exec sh -c 'umask 077; test -d ~/.ssh || mkdir ~/.ssh ; cat >> .ssh/authorized_keys'"
+
+if [ $# -eq 1 ]
+then
+  echo "installing public key for $1"
+  cat ~/.ssh/id_rsa.pub | ssh "$1" "exec sh -c 'umask 077; test -d ~/.ssh || mkdir ~/.ssh ; cat >> .ssh/authorized_keys'"
+else
+  echo "You need to specify a server name"
+fi
